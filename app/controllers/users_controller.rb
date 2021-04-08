041bcd1df03d09fd672aedcdec2710eb617d7425
miyamatakuya <1029takuya@gmail.com>
2021-04-08 23:44:18 +0900

    guest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.nickname = 'guest'
     end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
